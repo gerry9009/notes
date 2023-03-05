91f8b1c76c8b80c31bb84af8b4b3715d2d85dac8
Gergo Buglyo <buglyogergo@gmail.com>
2023-03-05 23:34:46 +0100

    Installed basic routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import { useState } from "react";
 import "./App.css";
+import { Routes, Route, Navigate } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Note App</h1>
-    </div>
+    <Routes>
+      <Route path="/" element={<h1>Home</h1>} />
+      <Route path="/new" element={<h1>New</h1>} />
+      <Route path="/:id">
+        <Route index element={<h1>Show</h1>} />
+        <Route path="edit" element={<h1>Edit</h1>} />
+      </Route>
+      <Route path="*" element={<Navigate to="/" />} />
+    </Routes>
   );
 }
 
